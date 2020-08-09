0ff64359e6bc8310da3a63b45aca6bbd4d7e85c4
jhys <1770613507@qq.com>
2020-08-09 21:18:33 +0800

    update

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,32 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"Hello mina",
+    num:10000,
+    isGirl:false,
+    person: {
+      age: 74,
+      height: 145,
+      weight: 200,
+      name: "富婆"
+    },
+    isChecked: false,
+    list:[
+      {
+        id:0,
+        name:"猪八戒"
+      },
+      {
+        id:1,
+        name:"天蓬元帅"
+      },
+      {
+        id:2,
+        name:"悟能"
+      }
+    ]
+  }
+})
