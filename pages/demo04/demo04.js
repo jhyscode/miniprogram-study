0f8797ebb2bed355c7423e7371ced238568d9dbe
jhys <1770613507@qq.com>
2020-08-12 21:22:15 +0800

    update

---
+++ pages/demo04/demo04.js
@@ -0,0 +1,26 @@
+// pages/demo04/demo04.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    num:0
+  },
+
+  //输入框的input事件的执行逻辑
+  handleInput(e) {
+    //console.log(e.detail.value);
+    this.setData({
+      num: e.detail.value
+    })
+  },
+ //加减按钮事件
+ handletap(e){
+   const operation = e.currentTarget.dataset.operation;
+   this.setData({
+    num: this.data.num + operation
+  })
+ }
+
+})
