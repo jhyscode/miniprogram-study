f1e9ae80c85b92177fa14be9ea3c906998d49f1d
jhys <1770613507@qq.com>
2020-08-13 21:11:13 +0800

    update

---
+++ pages/demo15/demo15.js
@@ -0,0 +1,23 @@
+// pages/demo15/demo15.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items:[
+      {name: 'USA', value: '美国'},
+      {name: 'CHN', value: '中国', checked: 'true'},
+      {name: 'BRA', value: '巴西'},
+      {name: 'JPN', value: '日本'},
+      {name: 'ENG', value: '英国'},
+      {name: 'TUR', value: '法国'},
+    ]
+  },
+
+  radioChange: function(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  }
+
+  
+})
